cd0c6e15dc5f3e45539d7d467ae15072ba1dbed7
prism <veronique.vdang@gmail.com>
2022-10-16 18:53:39 +0200

    camera is up, working well with flip camera and flash mode

--- app/screen/Camera_screen.js
+++ app/screen/Camera_screen.js
@@ -1,118 +1,165 @@
-import React, { useState } from "react";
-import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
-import * as Permissions from "expo-permissions";
-import { Camera } from "expo-camera";
-import AppIcon from '../components/appIcon/AppIcon';
-
+import React, { useEffect, useState, useRef } from "react";
+import { View, Text, TouchableOpacity, StyleSheet, Image } from "react-native";
+import Constants from 'expo-constants';
+import { Camera, CameraType } from "expo-camera";
+import * as MediaLibrary from "expo-media-library";
+import AppIcon from "../components/appIcon/AppIcon";
 
 const CameraScreen = () => {
-  // let cameraRef = useRef();
   const [hasCameraPermission, setHasCameraPermission] = useState(null);
   // const [hasMediaLibraryPermission, setHasMediaLibraryPermission] = useState(null);
-  // const [photo, setPhoto] = useState();
-  // const [type, setType] = useState(Camera.Constants.Type.back);
-  
-  const allowPermission = async () => {
-    try {
-        const camera = await Permissions.askAsync(Permissions.CAMERA)
-        if (!camera.granted) {
-            return Permissions.askAsync(Permissions.CAMERA)
+  const [Image, setImage] = useState(null);
+  const [typeCamera, setTypeCamera] = useState(Camera.Constants.Type.back);
+  const [flash, setFlash] = useState(Camera.Constants.FlashMode.off);
+  const cameraRef = useRef(null);
+
+  useEffect(() => {
+    (async () => {
+      MediaLibrary.requestPermissionsAsync();
+      const cameraStatus = await Camera.requestCameraPermissionsAsync();
+      setHasCameraPermission(cameraStatus.status === "granted");
+    })();
+  }, []);
+
+  const takePicture = async () => {
+    if (cameraRef) {
+        try {
+            const data = await cameraRef.current.takePictureAsync();
+            console.log(data);
+            setImage(data.uri);
+        } catch (err) {
+            console.log(err)
         }
-
-        setHasCameraPermission(true)
-
-    } catch {
-
-        console.log('error loading the camera')
     }
   }
 
-   if (hasCameraPermission === "undefined") {
-    return <Text>Requesting permissions ...</Text>;
-  } else if (!hasCameraPermission) {
-    return (
-        <View style={styles.notAllowed}>
-                 <TouchableOpacity 
-                style={styles.btn}
-                onPress={allowPermission}
-                >
-                    <Text style={styles.btnText}>Allow camera permission</Text>
-                </TouchableOpacity>
-              </View>
-    );
+  const savePicture = async () => {
+    if (Image) {
+      try {
+        const asset = await MediaLibrary.createAssetAsync(image);
+        alert('Picture saved! 🎉');
+        setImage(null);
+        console.log('saved successfully');
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
+  if (hasCameraPermission === false) {
+    return <Text>No access to camera</Text>
   }
+
+  const changeCameraType = () => {
+    if (typeCamera == "front") {
+      setTypeCamera("back");
+    } else if (typeCamera == "back") {
+      setTypeCamera("front");
+    } else {
+      setTypeCamera("front");
+    }
+  };
 
   return (
     <View style={{ flex: 1 }}>
-      <Camera style={{ flex: 1 }}>
-            <TouchableOpacity style={styles.captureBtn}></TouchableOpacity>
-                <View style={styles.header}>
-                    <AppIcon style={styles.headerIcons} AntName='user' color='#EB1D36' size={22} />
-                    <AppIcon style={styles.headerIcons} IonName="camera-reverse-outline" color="#EB1D36" size={22} />
-                </View>
+        {!Image ?
+      <Camera style={{ flex: 1 }} type={typeCamera} flashMode={flash} ref={cameraRef}>
+        <TouchableOpacity style={styles.captureBtn} onPress={takePicture}></TouchableOpacity>
+        <View style={styles.header}>
+          <AppIcon
+            style={styles.headerIcons}
+            AntName="user"
+            color="#EB1D36"
+            size={22}
+          />
+          <AppIcon
+            style={styles.headerIcons}
+            IonName="camera-reverse-outline"
+            color="#EB1D36"
+            size={22}
+            onPress={changeCameraType}
+          />
+        </View>
 
-                <View style={styles.sideItem}>
-                    <AppIcon style={styles.sideIcons} IonName="camera-outline" size={20} color="#EB1D36" />
-                    <AppIcon style={styles.sideIcons} IonName="flash-outline" size={20} color="#EB1D36" />
-                    {/* <AppIcon IonName="camera-outline" size={20} color="#EB1D36" /> */}
-                </View>
+        <View style={styles.sideItem}>
+          <AppIcon
+            style={styles.sideIcons}
+            IonName="camera-outline"
+            size={20}
+            color="#EB1D36"
+          />
+          <AppIcon
+            style={styles.sideIcons}
+            IonName="flash-outline"
+            size={20}
+            color={flash === Camera.Constants.FlashMode.off ? '#EB1D36' : '#f1f1f1'}
+            onPress={() => setFlash( flash === Camera.Constants.FlashMode.off
+            ? Camera.Constants.FlashMode.on 
+            : Camera.Constants.FlashMode.off
+            )}
+          />
+          {/* <AppIcon IonName="camera-outline" size={20} color="#EB1D36" /> */}
+        </View>
       </Camera>
+      :
+      <Image source={{uri: Image}} />
+    }
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-    notAllowed: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    btn: {
-        padding: 20,
-        backgroundColor: '#000',
-        justifyContent: 'center',
-        alignItems: 'center',
-        borderRadius: 10
-    },
-    btnText: {
-        color: '#eee',
-        fontSize: 18,
-        fontWeight: 'bold'
-    },
-    captureBtn: {
-        position: 'absolute',
-        bottom: 20,
-        width: 90,
-        height: 90,
-        borderRadius: 100,
-        borderBottomColor: '#EB1D36',
-        borderWidth: 6,
-        alignSelf: 'center'
-    },
-    header: {
-        position: 'absolute',
-        top: 40,
-        justifyContent: 'space-between',
-        padding: 15,
-        flexDirection: "row",
-        width: "100%"
-    },
-    headerIcons: {
-        width: 55,
-        height: 55
-    },
-    sideItem: {
-        position: "absolute",
-        top: 110,
-        right: 0,
-        padding: 15,
-    },
-    sideIcons: {
-        width: 50,
-        height: 50,
-        marginVertical: 8
-    }
-
-})
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    paddingTop: Constants.statusBarHeight
+  },
+  btn: {
+    padding: 20,
+    backgroundColor: "#000",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 10,
+  },
+  btnText: {
+    color: "#eee",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  captureBtn: {
+    position: "absolute",
+    bottom: 20,
+    width: 90,
+    height: 90,
+    borderRadius: 100,
+    borderBottomColor: "#EB1D36",
+    borderWidth: 6,
+    alignSelf: "center",
+  },
+  header: {
+    position: "absolute",
+    top: 40,
+    justifyContent: "space-between",
+    padding: 15,
+    flexDirection: "row",
+    width: "100%",
+  },
+  headerIcons: {
+    width: 55,
+    height: 55,
+  },
+  sideItem: {
+    position: "absolute",
+    top: 110,
+    right: 0,
+    padding: 15,
+  },
+  sideIcons: {
+    width: 50,
+    height: 50,
+    marginVertical: 8,
+  },
+});
 
 export default CameraScreen;
